05ace7e2b9e8a7717778410ebc40c436c547794e
Ivan Hartwig Bissoto <65921720+Hartwig99@users.noreply.github.com>
2021-01-21 20:45:53 -0300

    Update cabecalho.blade.php

--- resources/views/layout/cabecalho.blade.php
+++ resources/views/layout/cabecalho.blade.php
@@ -59,7 +59,7 @@
           <path d="M4 0h5.5v1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5h1V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2z"/>
           <path d="M9.5 3V0L14 4.5h-3A1.5 1.5 0 0 1 9.5 3z"/>
           <path fill-rule="evenodd" d="M4.5 12.5A.5.5 0 0 1 5 12h3a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5zm0-2A.5.5 0 0 1 5 10h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5zm1.639-3.708l1.33.886 1.854-1.855a.25.25 0 0 1 .289-.047l1.888.974V8.5a.5.5 0 0 1-.5.5H5a.5.5 0 0 1-.5-.5V8s1.54-1.274 1.639-1.208zM6.25 6a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5z"/>
-        </svg> Cadastrar Prescricao</a>
+        </svg> Cadastrar Prescrição</a>
         <a href="/cadastros/medicamento" class="list-group-item menu bg-success"> <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-plus-square-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
           <path fill-rule="evenodd" d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
         </svg>  Cadastrar Medicamento</a>
@@ -107,7 +107,7 @@
                   </li>
 
                   <li class="nav-item">
-                  <a class="nav-link menu-inicial" href="#">Configuracao </a>
+                  <a class="nav-link menu-inicial" href="/inicial/manual">Manual  </a>
                   </li>
 
                   <li class="nav-item">
