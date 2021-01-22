ad0412e29d3540cfa4bcc93d81f63d4acbc44e06
Ivan Hartwig Bissoto <65921720+Hartwig99@users.noreply.github.com>
2021-01-22 00:03:09 -0300

    Update sobre.blade.php

--- resources/views/inicial/sobre.blade.php
+++ resources/views/inicial/sobre.blade.php
@@ -10,8 +10,8 @@
         <h1> Sobre </h1>
         <span class="vc_empty_space_inner">
 
-            <p>Esse software foi criado com intencao de ajudar a  .....</p>
-            <p>É visto que a questão das rotinas administrativas foi desenvolvida para ter uma melhor gerência de medicamento e material
+            <p>Esse software foi criado com intenção de ajudar profissionais na Área da Saúde e Administrativa. </p>
+            <p>É visto que a questão das rotinas administrativas foi desenvolvida para ter uma melhor gerência de medicamentos e materiais
                que são receitados para pacientes na realização dos exames, sem um software de controle e gestão de dados, pesquisa de relatórios, sem armazenamento das informações das prescrições médicas, e isto dificulta a agilidade e qualidade da organização. no qual alguns profissionais ainda utilizam de planilhas Excel. </p>   
             <p>Os processos de rotinas administrativas podem trazer muitas informações importantes, para cada determinado órgão pode ser um tipo de procedimento mais complexo ou mais simples, o estudo realizado foi feito dentro de uma clínica,
                onde os processos são realizados para gerenciamento de medicamentos e materiais utilizados na realização dos exames e diagnóstico do paciente, no qual relata os produtos (medicamentos e materiais) utilizados no exame.</p>
@@ -45,13 +45,13 @@
     <div class="missao ">
     <h3 style="font-size: 36px;color: #0a0a0a;text-align: center;font-family:Dosis;font-style:normal" 
     class="vc_custom_heading">Missão</h3>
-    <p>Trazer para dentro das organizacões que realizam exames Diagnosticos, facilidade e rapidez em suas Prescricão Médica, alem de solucionar problemas presentes na gestão de medicamentos e materiais.  </p>
+    <p>Trazer para dentro das organizacões que realizam exames Diagnósticos, facilidade e rapidez em suas Prescrição Médica, além de trazer a possibilidade de solucionar problemas presentes na gestão de medicamentos e materiais.  </p>
     </div>
 
     <div class="visao ">
         <h3 style="font-size: 36px; text-align: center;font-family:Dosis;font-style:normal" 
         class="vc_custom_heading">Visão</h3>
-        <p>Ser até 2025, um dos softwares mais populares em gestao de administracao de medicamento e materiais, caracterizado pela resposabilidade e organização visando o cresminto e a melhor de funcionalidades e componetes.</p>
+        <p>Ser até 2025, um dos softwares mais populares em gestão de administração de medicamentos e materiais, caracterizado pela resposabilidade e organização visando o crescimento e o desenvolvimento de outras funcionalidades e componetes.</p>
     </div>
 
     <div class="valor ">
