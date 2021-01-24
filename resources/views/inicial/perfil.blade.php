9d95e2fe9a7b079347c747e1797d6271748e0422
Ivan Hartwig Bissoto <65921720+Hartwig99@users.noreply.github.com>
2021-01-24 18:12:47 -0300

    Add files via upload

---
+++ resources/views/inicial/perfil.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">  
+    <head>  
+        <meta charset="utf-8">
+       
+        <link href="{{ asset('css/cabecalho.css')}}"  rel="stylesheet">
+    </head>
+    <body>
+        
+  
+        @yield('conteudo')
+
+        
+ </body>
+</html>
