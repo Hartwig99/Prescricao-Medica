3d8541203810644d630976ae54a4375a03b42cde
Ivan Hartwig Bissoto <65921720+Hartwig99@users.noreply.github.com>
2021-01-24 23:26:37 -0300

    Add files via upload

--- resources/views/inicial/manual.blade.php
+++ resources/views/inicial/manual.blade.php
@@ -1,15 +1,44 @@
-<!DOCTYPE HTML>
-<html lang="pt-br">  
-    <head>  
-        <meta charset="utf-8">
-       
-        <link href="{{ asset('css/cabecalho.css')}}"  rel="stylesheet">
-    </head>
-    <body>
-        
-  
-        @yield('conteudo')
 
-        
- </body>
-</html>
+@extends('layout.cabecalho')
+
+@section('conteudo')
+<link href="{{ asset('css/manual.css')}}"  rel="stylesheet">
+<link href="{{ asset('css/cabecalho.css')}}"  rel="stylesheet">
+
+
+<form>
+<div class="texto">
+    <div >
+        <h1> Manual de utilização </h1>
+        <span class="vc_empty_space_inner">
+            <br>
+            <p>Por meio da tela de login o usuário pode entrar no sistema para que suas ações e operações no sistema sejam registradas no seu nome,
+                 além de possibilitar o usuário alterar a sua senha ou criar uma conta.</p>
+            <p>Ao  usuário realizar seu cadastro, ele é redirecionado à página inicial do sistema, 
+                onde ele tem algumas informações iniciais intuitivas, podendo acessar links que redirecionam para os sites onde ele poderá ler mais sobre o tópico escolhido.  </p>
+           
+            <p>Além disso o sistema disponibiliza uma barra de tarefas, onde ele poderá fazer contato com a empresa, ver informações sobre o site e realizar 
+                configurações para que possa ser mais produtivo ou até mesmo relatar problemas ocorrentes na ferramenta. 
+                A página inicial disponibiliza um menu de fácil acesso ao ser clicado nos 3 traços da representação do menu, 
+                no qual ele disponibiliza diversas outras opções que será comentado nas outras telas.</p>   
+                <br>  
+            <h4> Telas de Buscas</h4>
+            <br>    
+            <p>Tela de buscar material e medicamentos possui os campos exatos para as informações necessárias sobre a informação do produto e seu valor, no qual o usuário possui dois campos, podendo assim realizar a edição ou até mesmo a exclusão do material.
+                Quando realizado o Cadastro automaticamente ele já será inserido na tabela com suas informações para poder adicionar o produto utilizado na prescrição.
+                 Possui um campo para verificar as listas de materiais que são descartáveis ajudando o usuário ter uma informação ainda melhor sobre os produtos.</p>
+                 <br>  
+            <h4> Tela de Prescrição Médica </h4>     
+            <br>   
+            <p> Já na tela de Realizar Prescrição Médica é possível o profissional adicionar todos as informações necessárias sobre o que foi utilizado na realização do exame, além de conter os dados dos medicamentos e materiais que foram utilizados. 
+                Possui campos para inserir dados do paciente, convênio, data de realização do exame, observação no qual é inserido , como foi realizado o exame e o que foi utilizado, entre outras informações necessárias que o profissional precisa adicionar. </p>
+
+            <p> Esta possui 3 botões que o usuário pode utilizar para adicionar a quantidade certa de medicamentos e materiais que foram utilizados no exame, fazendo uma busca geral na base de dados. </p>    
+            <a href="/doc/manual.docx" target='_blank' 
+            title="Clique aqui para abrir um documento do Word">
+            Clique aqui para abrir o Manual Completo do Sistema </a>
+
+        </div>        
+    </form> 
+
+    @endsection
--- resources/views/inicial/perfil.blade.php
+++ resources/views/inicial/perfil.blade.php
@@ -1,15 +1,118 @@
-<!DOCTYPE HTML>
-<html lang="pt-br">  
-    <head>  
-        <meta charset="utf-8">
-       
-        <link href="{{ asset('css/cabecalho.css')}}"  rel="stylesheet">
-    </head>
-    <body>
-        
-  
-        @yield('conteudo')
 
+@extends('layout.cabecalho')
+
+@section('conteudo')
+<link href="{{ asset('css/perfil.css')}}"  rel="stylesheet">
+<link href="{{ asset('css/cabecalho.css')}}"  rel="stylesheet">
+
+<form class="texto">
+    <div class="form-row">
+        <div class="form-group col-md-10">
+          <label for="inputEmail4"> Endereço de Email</label>
+          <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+        </div>
+        <div class="form-group col-md-6">
+          <label for="inputPassword4">Senha</label>
+          <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+        </div>
+      </div>
+      <div class="form-group">
+        <label for="inputAddress">Endereço</label>
+        <input type="text" class="form-control" id="inputAddress" placeholder="Av. Brasil">
+      </div>
+      <div class="form-group">
+        <label for="inputAddress2">Complemento</label>
+        <input type="text" class="form-control" id="inputAddress2" placeholder="Perto do Mercado Mega">
+      </div>
+      <div class="form-row">
+        <div class="form-group col-md-6">
+          <label for="inputCity">Cidade</label>
+          <input type="text" class="form-control" id="inputCity">
+        </div>
+        <div class="form-group col-md-4">
+          <label for="inputState">Estado</label>
+          <select id="inputState" class="form-control">
+            <option selected>Selecione...</option>
+            <option>Acre(AC)</option>
+            <option>Alagoas(AL)</option>
+            <option>Amapa(AP)</option>
+            <option>Amaoznas(AM)</option>
+            <option>Bahia(BA)</option>
+            <option>Ceará(CE)</option>
+            <option>Distrito Federal (DF)</option>
+            <option>Espírito Santo (ES)</option>
+            <option>Goiás (GO)</option>
+            <option>Maranhão (MA)</option>
+            <option>Mato Grosso (MT)</option>
+            <option>Mato Grosso do Sul (MS)</option>
+            <option>Minas Gerais (MG)</option>
+            <option>Pará (PA)</option>
+            <option>Paraíba (PB)</option>
+            <option>Paraná (PR)</option>
+            <option>Tocantins (TO)</option>
+            <option>Sergipe (SE)</option>
+            <option>São Paulo (SP)</option>
+            <option>Santa Catarina (SC)</option>
+            <option>Roraima (RR)</option>
+            <option>Rio Grande do Sul (RS)</option>
+            <option>Rio Grande do Norte (RN)</option>
+            <option>Rio de Janeiro (RJ)</option>
+            <option>Pernambuco (PE)</option>
+            <option>Piauí (PI)</option>
+           
+          </select>
+        </div>
+        <div class="form-group col-md-4">
+          <label for="inputZip">CEP</label>
+          <input type="text" class="form-control" id="inputZip">
+        </div>
+
+        <div class="form-group col-md-4">
+            <label for="inputZip">CRM</label>
+            <input type="text" class="form-control"  placeholder="Opcional" >
+          </div>
+      </div>
+      <div class="form-group">
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" id="gridCheck">
+          <label class="form-check-label" for="gridCheck">
+            Dr.
+          </label>
+        </div>
+          <div class="form-check">
+          <input class="form-check-input" type="checkbox" id="gridCheck">
+          <label class="form-check-label" for="gridCheck">
+            Dra.
+          </label>
+          </div>
+          <div class="form-check">
+          <input class="form-check-input" type="checkbox" id="gridCheck">
+          <label class="form-check-label" for="gridCheck">
+            Gerente.
+          </label>
+          </div>
         
- </body>
-</html>
+      </div>
+      <button type="submit" class="btn btn-primary">Salvar</button>
+</div>  
+
+<div class="espaco2" >
+        <div>
+        <h2>Carregar Foto do Perfil </h2>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="/img/ihb.ico" alt="Card image cap">
+        </div>    
+    <form>
+        
+        <br>
+        <div class="form-group">
+          <label for="exampleFormControlFile1">Escolha o arquivo png ou jpg</label>
+          <input type="file" class="form-control-file" id="exampleFormControlFile1">
+        </div>
+      </form>
+</div>    
+
+</form>
+
+@endsection
