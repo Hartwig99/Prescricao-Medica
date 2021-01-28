5120fcd8dd70821d44faeb5f72b3d890ee842be7
Ivan Hartwig Bissoto <65921720+Hartwig99@users.noreply.github.com>
2021-01-28 00:32:17 -0300

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -36,7 +36,7 @@
                         
                        
                         <div class="form-group row">
-                            <label class="control-label col-md-4 col-form-label text-md-right" id="campo">CPF</label>
+                            <label for="cpf" class="control-label col-md-4 col-form-label text-md-right" >CPF</label>
                             <div class="col-sm-6">
                             <input id="cpf" type="text" class="form-control form-control-user" name="cpf" maxlength="14" placeholder="Ex.: 000.000.000-00" required />
                             </div>
