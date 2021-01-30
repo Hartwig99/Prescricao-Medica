a8c68cf1cdbebf41ab14a8854f51054e905cb932
Ivan Hartwig Bissoto <65921720+Hartwig99@users.noreply.github.com>
2021-01-30 00:02:22 -0300

    Update manual.blade.php

--- resources/views/inicial/manual.blade.php
+++ resources/views/inicial/manual.blade.php
@@ -14,13 +14,11 @@
             <br>
             <p>Por meio da tela de login o usuário pode entrar no sistema para que suas ações e operações no sistema sejam registradas no seu nome,
                  além de possibilitar o usuário alterar a sua senha ou criar uma conta.</p>
-            <p>Ao  usuário realizar seu cadastro, ele é redirecionado à página inicial do sistema, 
-                onde ele tem algumas informações iniciais intuitivas, podendo acessar links que redirecionam para os sites onde ele poderá ler mais sobre o tópico escolhido.  </p>
+            <p>Ao  usuário realizar seu cadastro, é redirecionado à página inicial do sistema com algumas informações iniciais intuitivas, podendo acessar links que redirecionam para os sites com informações que podem ser lidas sobre o tópico escolhido.  </p>
            
-            <p>Além disso o sistema disponibiliza uma barra de tarefas, onde ele poderá fazer contato com a empresa, ver informações sobre o site e realizar 
+            <p>Além disso, o sistema disponibiliza uma barra de tarefas na qual o usuário conseguirá fazer contato, ver informações sobre o site e realizar 
                 configurações para que possa ser mais produtivo ou até mesmo relatar problemas ocorrentes na ferramenta. 
-                A página inicial disponibiliza um menu de fácil acesso ao ser clicado nos 3 traços da representação do menu, 
-                no qual ele disponibiliza diversas outras opções que será comentado nas outras telas.</p>   
+                A página inicial ofere um menu de fácil acesso ao ser clicado nos 3 traços da representação do menu que viabiliza diversas outras opções comentadas nas outras telas.</p>   
                 <br>  
             <h4> Telas de Buscas</h4>
             <br>    
@@ -31,7 +29,7 @@
             <h4> Tela de Prescrição Médica </h4>     
             <br>   
             <p> Já na tela de Realizar Prescrição Médica é possível o profissional adicionar todos as informações necessárias sobre o que foi utilizado na realização do exame, além de conter os dados dos medicamentos e materiais que foram utilizados. 
-                Possui campos para inserir dados do paciente, convênio, data de realização do exame, observação no qual é inserido , como foi realizado o exame e o que foi utilizado, entre outras informações necessárias que o profissional precisa adicionar. </p>
+                Possui campos para inserir dados do paciente, convênio, data de realização do exame, observação no qual é inserido, como foi realizado o exame  e quais medicamentos e materiais foram utilizados, entre outras informações necessárias que o profissional precisa adicionar. </p>
 
             <p> Esta possui 3 botões que o usuário pode utilizar para adicionar a quantidade certa de medicamentos e materiais que foram utilizados no exame, fazendo uma busca geral na base de dados. </p>    
             <a href="/doc/manual.docx" target='_blank' 
